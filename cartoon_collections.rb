f6e0d271278d4f666b9fb769f41b372595c0e9d3
Geoffrey Harrison <harrisongeo87@gmail.com>
2020-03-27 02:55:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,6 +20,6 @@ end
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_types.each do |cheese|
+  cheese_types.each_with_index do |cheese|
     array.include?
 end
