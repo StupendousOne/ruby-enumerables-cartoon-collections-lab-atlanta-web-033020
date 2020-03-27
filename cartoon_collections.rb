2df53eb5e298deac79c1c67a59e5b7815ed83084
Geoffrey Harrison <harrisongeo87@gmail.com>
2020-03-27 02:41:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ end
 
 def long_planeteer_calls(calls)# code an argument here
   # Your code here
-  calls.reduce{|call| call.length > 4}
+  calls.include?{|call| call.length > 4}
 end
 
 def find_the_cheese# code an argument here
