69037c4299fef24ebbd8bba7735fd1abbf236c68
Geoffrey Harrison <harrisongeo87@gmail.com>
2020-03-27 03:03:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,7 @@
 def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
-  dwarves.each {|dwarf| puts dwarf}
+  list_num = 1
+  dwarves.each {|dwarf| puts "#{dwarf}"}
 end
 
 def summon_captain_planet(elements)# code an argument here
